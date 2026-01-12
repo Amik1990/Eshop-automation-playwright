a2db77d0be7544c68deb39678b8f752db3b483e1
Amik1990 <djarnot@seznam.cz>
2026-01-12 18:49:34 +0100

    nova metoda click_signup_login()

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import expect
 
 from .base_page import BasePage
@@ -5,7 +7,6 @@ from utils.config import config
 
 
 class HomePage(BasePage):
-
 
     def load(self, url: str = f"{config.BASE_URL}") -> None:
         self.LOG.info(f"Načítám stránku {url}")
@@ -15,3 +16,13 @@ class HomePage(BasePage):
         automationExercise = self.page.get_by_role("heading", name="AutomationExercise")
         self.expect_visible(automationExercise, name="Title Automation Exercise")
         self.LOG.info(f"Title Automation Exercise je viditelný.")
+
+    def click_signup_login (self):
+        signup_login_button = self.page.get_by_role("link", name=" Signup / Login")
+        self.click(signup_login_button, name="Tlačítko Sign Up / Login")
+
+        expect(self.page).to_have_url(re.compile("https://automationexercise.com/login"))
+        self.LOG.info(f"Úspěšně přesměrováno na Login stránku.")
+
+
+
