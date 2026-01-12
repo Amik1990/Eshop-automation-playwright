a8472e90b5b32de1aff8f576bcb8e1443eb9d7ab
Amik1990 <djarnot@seznam.cz>
2026-01-12 18:51:27 +0100

    update testu test_register_user

--- tests/testRegisterUser.py
+++ tests/testRegisterUser.py
@@ -1,4 +1,11 @@
 from pages import HomePage, LoginPage
+from pages import base_page
 
 def test_register_user(load_home_page: HomePage):
     load_home_page.load()
+    load_home_page.click_signup_login()
+
+
+
+
+
