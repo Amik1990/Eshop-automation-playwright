b73e0f5718a0870b7bef4251ecbd2c9f99511767
Amik1990 <djarnot@seznam.cz>
2026-01-27 19:13:21 +0100

    novy test test_logout_user.py

--- tests/test_logout_user.py
+++ tests/test_logout_user.py
@@ -1,25 +1,28 @@
 from pages import HomePage, LoginPage
 
-def test_incorrect_login(load_home_page: HomePage, user_data):
+def test_logout_user(load_home_page: HomePage, registered_user):
     """
     Test ověří přihlášení existujícího uživatele.
     Uživatel je vytvořen fixturou 'registered_user'.
     """
     # 1. Získání dat z fixtury
-    email = user_data["email"]
-    password = user_data["password"]
+    email = registered_user["email"]
+    password = registered_user["password"]
+    name = registered_user["name"]
 
     # 2. Přejdeme na login stránku
     load_home_page.click_signup_login()
 
-    # 3. Provedeme login s nevalidnimi daty
+    # 3. Provedeme login
     login_page = LoginPage(load_home_page.page)
     login_page.expect_visible(login_page.loginToYourAccount, name="Login to your account")
     login_page.login_user(email, password)
 
-    #4. Ověříme, že se zobrazila error hláška: Your email or password is incorrect!
-    login_page.verify_incorrect_login()
+    #4. Ověříme, že jsme přihlášeni
+    load_home_page.verify_logged_in_user(name)
 
-    print("END OF TEST: INCORRECT LOGIN")
+    # 5. Odhlášení účtu
+    load_home_page.click_logout()
+    print("END OF TEST: LOGOUT USER")
 
 
